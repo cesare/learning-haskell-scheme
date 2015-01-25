d9e5b8ebcaeb17571eaa6879ff3caf77a749fcc4
SAWADA Tadashi <cesare@mayverse.jp>
2015-01-25 13:46:16 +0900

    Add the readExpr function

--- src/Scheme.hs
+++ src/Scheme.hs
@@ -6,3 +6,7 @@ import System.Environment
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
