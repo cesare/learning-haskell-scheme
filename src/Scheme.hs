825ac9b792a6869fcc3e36df296414536b3131c2
SAWADA Tadashi <cesare@mayverse.jp>
2015-01-25 14:27:04 +0900

    Add the parseString

--- src/Scheme.hs
+++ src/Scheme.hs
@@ -17,6 +17,13 @@ spaces = skipMany1 space
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
 readExpr :: String -> String
 readExpr input = case parse (spaces >> symbol) "lisp" input of
   Left err -> "No match: " ++ show err
